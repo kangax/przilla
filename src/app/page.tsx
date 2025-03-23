24537b42b97f20c8d98817344f732f6125f70c2a
Juriy Zaytsev <kangax@gmail.com>
2025-03-23 08:28:30 -0700

    Move header to the fixed bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Box, Container, Flex, Heading } from "@radix-ui/themes";
+import { Box, Container, Flex, Heading, Text } from "@radix-ui/themes";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 
@@ -28,11 +28,23 @@ export default async function Home() {
   return (
     <HydrateClient>
       <Box className="min-h-screen bg-[#09090b]">
-        <Container size="3" className="py-8">
-          <Flex direction="column" gap="6">
-            <Heading size="6" align="center">
-              WOD Pain Wall
+        {/* Fixed Top Bar */}
+        <Box className="fixed top-0 left-0 w-full bg-[#09090b] py-4 px-4 z-10 border-b border-gray-800">
+          <Flex >
+            <Heading size="5" align="center">
+                PRzilla
             </Heading>
+            {/* Statistics display */}
+            <Flex justify="center" gap="4" className="text-gray-400 text-sm ml-auto" align="center">
+              <Text>{wodsData.length} WODs</Text>
+              <Text>{wodsData.reduce((total, wod) => total + wod.results.length, 0)} Sessions</Text>
+            </Flex>
+          </Flex>
+        </Box>
+        
+        {/* Main Content with top margin to account for fixed header */}
+        <Container size="3" className="py-8 pt-16">
+          <Flex direction="column" gap="6">
             
             <WodViewer wods={wodsData} />
             
